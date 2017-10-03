1760991549f4ad62455c554496bb779e28cab473
Hein Htet <heinhtet.hh405@gmail.com>
2017-10-03 14:00:46 +0630

    InitialCommit

---
+++ include/config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Database configuration
+ */
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'gcm_chat');
+ 
+define("GOOGLE_API_KEY", "AIzaSyA9Ze27IUIAi3Jkr1yHqPp9t8IEt3ASSO8");
+ 
+// push notification flags
+define('PUSH_FLAG_CHATROOM', 1);
+define('PUSH_FLAG_USER', 2);
